f0867290a4452ff56aa4658eb9483970cce38fc9
Filippo <filippo.nardini@studio.unibo.it>
2024-05-26 13:38:04 +0200

    feat: better lippo0 layout

---
+++ keyboards/handwired/lippo0/keymaps/default/keymap.c
@@ -0,0 +1,21 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+                            MO(1), KC_LSFT, KC_LALT,  KC_SPC
+    ),
+    [1] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+                          _______, _______, _______, _______
+    )
+};
